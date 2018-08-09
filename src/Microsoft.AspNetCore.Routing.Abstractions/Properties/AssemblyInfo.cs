46973b70a9922208c4dc413ebd3b094fe2298152
James Newton-King <james@newtonking.com>
2018-08-09 20:13:50 +1200

    Derp

--- src/Microsoft.AspNetCore.Routing.Abstractions/Properties/AssemblyInfo.cs
+++ src/Microsoft.AspNetCore.Routing.Abstractions/Properties/AssemblyInfo.cs
@@ -1,9 +1,9 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System.Runtime.CompilerServices;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.AspNetCore.Routing.Features;
-using System.Runtime.CompilerServices;
 
 [assembly: TypeForwardedTo(typeof(IEndpointFeature))]
 [assembly: TypeForwardedTo(typeof(Endpoint))]
